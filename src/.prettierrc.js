694be1802b436e32be6d22d9482f8930a462a463
David Gomez <david.gomez@landbot.io>
2024-09-02 17:04:37 +0200

    fix(prettier): fix prettier rules

--- src/.prettierrc.js
+++ src/.prettierrc.js
@@ -1,8 +1,13 @@
 module.exports = {
-  singleQuote: true,
-  printWidth: 120,
-  plugins: [require.resolve('@trivago/prettier-plugin-sort-imports')],
+  arrowParens: 'avoid',
+  bracketSpacing: true,
   importOrder: ['^@(.*)$', '^[a-zA-Z]*/(.*)$', '^[./]'],
   importOrderSeparation: true,
   importOrderSortSpecifiers: true,
+  plugins: [require.resolve('@trivago/prettier-plugin-sort-imports')],
+  printWidth: 120,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
 };
